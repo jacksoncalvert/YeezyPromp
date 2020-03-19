edf45e1f3985b5048abe6b6799aacf89f02ece6e
jacksoncalvert <jcalvertlane97@gmail.com>
2020-03-19 22:21:33 +1000

    day 1 complete

--- scrape.py
+++ scrape.py
@@ -2,14 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-url = "https://www.chrisburkard.com/"
+url = "https://www.adidas.com.au/yeezy"
 
-web_r = requests.get(url)
-web_soup = BeautifulSoup(web_r.text, 'html.parser')
+#web_r = requests.get(url)
+#web_soup = BeautifulSoup(web_r.text, 'html.parser')
 
-print(len(web_soup.findAll("img")))
+#print(len(web_soup.findAll("img")))
 
 
 driver = webdriver.Firefox()
-driver.get("https://www.adidas.com.au/yeezy")
-	
+driver.get(url)
+
+html = driver.execute_script("return document.documentElement.outerHTML")
+sel_soup = BeautifulSoup(html, 'html.parser')
+
+print(sel_soup.('Cinder'))
+
+  
